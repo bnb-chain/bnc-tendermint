41fe77d74d345741e2974196eebf8670af6c377d
fudongbai <296179868@qq.com>
2018-12-24 10:06:49 +0800

    fix sentry node replay faild

--- consensus/replay.go
+++ consensus/replay.go
@@ -323,7 +323,7 @@ func (h *Handshaker) ReplayBlocks(
 
 	// First handle edge cases and constraints on the storeBlockHeight.
 	if storeBlockHeight == 0 {
-		return appHash, checkAppHash(state, appHash)
+		return appHash, checkAppHash(state, appHash, !h.withAppStat)
 
 	} else if storeBlockHeight < appBlockHeight {
 		// the app should never be ahead of the store (but this is under app's control)
@@ -350,7 +350,7 @@ func (h *Handshaker) ReplayBlocks(
 
 		} else if appBlockHeight == storeBlockHeight {
 			// We're good!
-			return appHash, checkAppHash(state, appHash)
+			return appHash, checkAppHash(state, appHash, !h.withAppStat)
 		}
 
 	} else if storeBlockHeight == stateBlockHeight+1 {
@@ -425,7 +425,7 @@ func (h *Handshaker) replayBlocks(state sm.State, proxyApp proxy.AppConns, appBl
 		appHash = state.AppHash
 	}
 
-	return appHash, checkAppHash(state, appHash)
+	return appHash, checkAppHash(state, appHash, h.withAppStat)
 }
 
 // ApplyBlock on the proxyApp with the last block.
@@ -446,7 +446,10 @@ func (h *Handshaker) replayBlock(state sm.State, height int64, proxyApp proxy.Ap
 	return state, nil
 }
 
-func checkAppHash(state sm.State, appHash []byte) error {
+func checkAppHash(state sm.State, appHash []byte, skip bool) error {
+	if skip {
+		return nil
+	}
 	if !bytes.Equal(state.AppHash, appHash) {
 		panic(fmt.Errorf("Tendermint state.AppHash does not match AppHash after replay. Got %X, expected %X", appHash, state.AppHash).Error())
 	}
