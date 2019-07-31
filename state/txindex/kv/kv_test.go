602c046f1961185f435bde06e5410c3654a4b3fe
Alexander Bezobchuk <alexanderbez@users.noreply.github.com>
2019-07-31 08:01:55 -0700

    txindexer: Refactor Tx Search Aggregation (#3851)
    - Replace the previous intersect call, which was called at each query condition, with a map intersection.
    - Replace fmt.Sprintf with string()
    
    closes: #3076
    
    Benchmarks
    
    ```
    Old
    goos: darwin
    goarch: amd64
    pkg: github.com/tendermint/tendermint/state/txindex/kv
    BenchmarkTxSearch-4   	     200	 103641206 ns/op	 7998416 B/op	   71171 allocs/op
    PASS
    ok  	github.com/tendermint/tendermint/state/txindex/kv	26.019s
    
    New
    goos: darwin
    goarch: amd64
    pkg: github.com/tendermint/tendermint/state/txindex/kv
    BenchmarkTxSearch-4   	    1000	  38615024 ns/op	13515226 B/op	  166460 allocs/op
    PASS
    ok  	github.com/tendermint/tendermint/state/txindex/kv	53.618s
    ```
    
    ~62% performance improvement
    
    Commits:
    
    * Refactor tx search
    
    * Add pending changelog entry
    
    * Add tx search benchmarking
    
    * remove intermediate hashes list
    
    also reset timer in BenchmarkTxSearch
    and fix other benchmark
    
    * fix import
    
    * Add test cases
    
    * Fix searching
    
    * Replace fmt.Sprintf with string
    
    * Update state/txindex/kv/kv.go
    
    Co-Authored-By: Anton Kaliaev <anton.kalyaev@gmail.com>
    
    * Rename params
    
    * Cleanup
    
    * Check error in benchmarks

--- state/txindex/kv/kv_test.go
+++ state/txindex/kv/kv_test.go
@@ -12,6 +12,8 @@ import (
 	cmn "github.com/tendermint/tendermint/libs/common"
 	db "github.com/tendermint/tendermint/libs/db"
 
+	abci "github.com/tendermint/tendermint/abci/types"
+	cmn "github.com/tendermint/tendermint/libs/common"
 	"github.com/tendermint/tendermint/libs/pubsub/query"
 	"github.com/tendermint/tendermint/state/txindex"
 	"github.com/tendermint/tendermint/types"
@@ -89,6 +91,11 @@ func TestTxSearch(t *testing.T) {
 		{"account.number = 1 AND account.owner = 'Ivan'", 1},
 		// search by exact match (two tags)
 		{"account.number = 1 AND account.owner = 'Vlad'", 0},
+		{"account.owner = 'Vlad' AND account.number = 1", 0},
+		{"account.number >= 1 AND account.owner = 'Vlad'", 0},
+		{"account.owner = 'Vlad' AND account.number >= 1", 0},
+		{"account.number <= 0", 0},
+		{"account.number <= 0 AND account.owner = 'Ivan'", 0},
 		// search using a prefix of the stored value
 		{"account.owner = 'Iv'", 0},
 		// search by range
@@ -310,7 +317,7 @@ func benchmarkTxIndex(txsCount int64, b *testing.B) {
 	}
 	defer os.RemoveAll(dir) // nolint: errcheck
 
-	store := db.NewDB("tx_index", "leveldb", dir)
+	store := db.NewDB("tx_index", "goleveldb", dir)
 	indexer := NewTxIndex(store)
 
 	batch := txindex.NewBatch(txsCount)
